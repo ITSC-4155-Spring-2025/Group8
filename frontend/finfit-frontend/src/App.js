f10110387d3ccdaf4f06541d34b7c169e46d07f9
Shlok Singh <ssingh67@uncc.edu>
2025-02-16 21:03:50 -0500

    Created User and Leaderboard

--- frontend/finfit-frontend/src/App.js
+++ frontend/finfit-frontend/src/App.js
@@ -1,5 +1,7 @@
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import  { User }  from './pages/User';
+import  Leaderboard  from './pages/Leaderboard';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -9,10 +11,13 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          <Route path = "/User" element = {<User />}/>
+          <Route path = "/Leaderboard" element = {<Leaderboard />}/>
         </Routes>
       </Router>
     </div>
-  );
+  )
+  export default App;
 }
 
-export default App; 
+
