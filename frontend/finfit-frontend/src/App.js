6269f7a6351548fd513895c13f85d48618095aaa
Shlok Singh <ssingh67@uncc.edu>
2025-02-16 21:03:50 -0500

    Created User and Leaderboard

--- frontend/finfit-frontend/src/App.js
+++ frontend/finfit-frontend/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+
+import {HashRouter as Router, Routes, Route} from 'react-router-dom'
+import  { User }  from './pages/User';
+import  Leaderboard  from './pages/Leaderboard';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path = "/User" element = {<User />}/>
+        <Route path = "/Leaderboard" element = {<Leaderboard />}/>
+      </Routes>
+    </Router>
   );
 }
 
