ecd5f56616546c8be37aa7779e286dac7b6cebc4
estark3 <estark3@charlotte.edu>
2025-03-17 23:20:33 -0400

    Created branch for the backend database and slightly modified settings.py

--- backend/finfit_backend/settings.py
+++ backend/finfit_backend/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'finfit_backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'finfit_tables',
+        'USER': 'root',
+        'PASSWORD': 'softwareEGR',      # May need to change password depending on who's running it.
+        'HOST': 'localhost'
     }
 }
 
