8f3402e54a91bde6cf00a3a1d7ee5419f39af29d
Shlok Singh <ssingh67@uncc.edu>
2025-04-08 22:01:10 -0400

    user authentication

--- frontend/finfit-frontend/src/pages/Home.js
+++ frontend/finfit-frontend/src/pages/Home.js
@@ -4,6 +4,7 @@ import Footer from '../components/Footer';
 import Purpose from '../components/Home/Purpose';
 import Features from '../components/Home/Features';
 import Testimonial from '../components/Home/Testimonials';
+import LoginForm from '../components/LoginForm';
 
 function Home() {
   return (
